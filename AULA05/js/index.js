5c5db198cda1349907afb9c8cdc974eabb906ce2
luisveber <luisveber@hotmail.com>
2022-08-18 21:30:12 -0300

    AULA05

---
+++ AULA05/js/index.js
@@ -0,0 +1,4 @@
+console.log('Ola Mundo');
+console.log('Este Trecho será exibido no console do navegador');
+console.log('Hoje é dia ', 18, 'de agosto de ', 2022);
+alert('Isso é um alerta!!!');
---
+++ AULA06/index.js
@@ -0,0 +1,11 @@
+let nome = 'Luis';
+let esposa = 'Suziane';
+let filho = 'Miguel';
+let anoNascimento = 1987;
+
+
+console.log(nome, 'nasceu em', anoNascimento);
+console.log('Em 2000', nome, 'conheceu', esposa);
+console.log(nome, 'casou-se com', esposa ,'em 2012');
+console.log(esposa, 'teve um filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama', filho);
