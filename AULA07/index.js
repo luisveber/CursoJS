6c13aec97f3a3566ef14bdadd419ed529256114a
luisveber <luisveber@hotmail.com>
2022-08-21 12:14:57 -0300

    AULA07

---
+++ AULA07/index.js
@@ -0,0 +1,6 @@
+const PrimeiroNumero = 5;
+const SegundoNumero = 10;
+const Resultado = PrimeiroNumero * SegundoNumero;
+const ResultadoDuplicado = Resultado * 2;
+
+console.log(ResultadoDuplicado);
